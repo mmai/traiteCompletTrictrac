0fca48e5102ea3c3b8045b809c76e11b3944f41e
Henri Bourcereau <henri.bourcereau@gmail.com>
2024-07-23 21:36:08 +0200

    material-plausible mkdocs plugin

--- devenv.nix
+++ devenv.nix
@@ -1,15 +1,28 @@
 { pkgs, ... }:
-# { pkgs, stdenv, ... }:
+let
+  pythonPackages = pkgs.python3Packages;
+in
 {
-  # https://devenv.sh/basics/
-  # env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
   packages = [
     pkgs.python312Packages.mkdocs
     pkgs.python312Packages.mkdocs-material
 
     pkgs.imagemagick # to convert svg to png
     # pkgs.calibre # for tests : epub viewer
+    (
+      pythonPackages.buildPythonPackage rec {
+        pname = "material_plausible_plugin";
+        version = "0.2.0";
+        format = "wheel";
+        src = pythonPackages.fetchPypi rec {
+          inherit pname version format;
+          sha256 = "b7dc866b358475d940c5c61f56f86c400b9c1e73ffa2b06819207df38f34fcf4";
+          dist = python;
+          python = "py3";
+          #abi = "none";
+          #platform = "any";
+        };
+
+      })
   ];
 }
