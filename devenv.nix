f88cbcf5b46088c2340f81028f08c4b2f111fbea
Henri Bourcereau <henri.bourcereau@gmail.com>
2024-07-24 21:49:37 +0200

    plausible analytics

--- devenv.nix
+++ devenv.nix
@@ -1,28 +1,29 @@
 { pkgs, ... }:
 let
-  pythonPackages = pkgs.python3Packages;
+  pythonPackages = pkgs.python312Packages;
 in
 {
   packages = [
-    pkgs.python312Packages.mkdocs
-    pkgs.python312Packages.mkdocs-material
+    pythonPackages.mkdocs
+    pythonPackages.mkdocs-material
 
     pkgs.imagemagick # to convert svg to png
     # pkgs.calibre # for tests : epub viewer
-    (
-      pythonPackages.buildPythonPackage rec {
-        pname = "material_plausible_plugin";
-        version = "0.2.0";
-        format = "wheel";
-        src = pythonPackages.fetchPypi rec {
-          inherit pname version format;
-          sha256 = "b7dc866b358475d940c5c61f56f86c400b9c1e73ffa2b06819207df38f34fcf4";
-          dist = python;
-          python = "py3";
-          #abi = "none";
-          #platform = "any";
-        };
-
-      })
+    # (
+    #   pythonPackages.buildPythonPackage rec {
+    #     pname = "material-plausible-plugin";
+    #     version = "0.2.0";
+    #     format = "wheel";
+    #     src = pythonPackages.fetchPypi rec {
+    #       inherit version format;
+    #       pname = "material_plausible_plugin";
+    #       sha256 = "b7dc866b358475d940c5c61f56f86c400b9c1e73ffa2b06819207df38f34fcf4";
+    #       dist = python;
+    #       python = "py3";
+    #       #abi = "none";
+    #       #platform = "any";
+    #     };
+    #
+    #   })
   ];
 }
